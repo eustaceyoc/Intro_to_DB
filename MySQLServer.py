7affaf76f6e3076e5b3ee1e25691a5ca202e276f
Eustace Crentsil <crentsil.eustace25@gmail.com>
2025-12-14 10:10:15 +0000

    made rubric corrections to MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
@@ -10,16 +9,15 @@ def create_database():
             password="trueblue"
         )
 
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-            print("Database 'alx_book_store' created successfully!")
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print("Error while connecting to MySQL:", e)
 
     finally:
-        if connection is not None and connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()
 
